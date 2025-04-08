454556f01e741b071cd128c0ff9f997562a848dd
dwiajisobarna <dwiajisobarna1@gmail.com>
2025-04-08 11:39:28 +0700

    Initial Commit

---
+++ NoteManager/NoteManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  NoteManagerApp.swift
+//  NoteManager
+//
+//  Created by Dwi Aji Sobarna on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
